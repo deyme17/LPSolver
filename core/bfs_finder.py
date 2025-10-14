72e7a3eff0f8a70c8e732ff3a822da5bbe1d520b
dmytro <dmytro19022006@gmail.com>
2025-10-14 16:43:43 +0300

    BFSFinder carcass

---
+++ core/bfs_finder.py
@@ -0,0 +1,13 @@
+from utils import LPProblem, BFSolution
+
+class BFSFinder:
+    @staticmethod
+    def find_initial_bfs(standard_form: LPProblem) -> BFSolution:
+        """
+        Computes an initial basic feasible solution for linear programming problems.
+        Args:
+            standard_form (LPProblem): The linear programming problem in standard form
+        Returns:
+            BFSolution: Basic feasible solution
+        """
+        pass

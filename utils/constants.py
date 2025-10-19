013783ad8cd10bd9db8b8340c08ebd053e45b87c
dmytro <dmytro19022006@gmail.com>
2025-10-19 16:37:47 +0300

    upsize width

--- utils/constants.py
+++ utils/constants.py
@@ -3,7 +3,7 @@ from enum import Enum
 # app
 class AppConstants:
     WINDOW_TITLE = "Linear Programming Solver"
-    WINDOW_SIZE = (800, 700)
+    WINDOW_SIZE = (900, 700)
     TITLE_FONT_SIZE = 16
     BUTTON_HEIGHT = 40
     BUTTON_FONT_SIZE = 12

5ec32346679b2da436e9a2413822bb88487cd6b5
dmytro <dmytro19022006@gmail.com>
2025-10-19 15:33:28 +0300

    fix BasicBFSFinder (indexing problem)

--- core/bfs_finder.py
+++ core/bfs_finder.py
@@ -2,15 +2,15 @@ from utils import LPProblem, BFSolution, IBFSFinder
 
 
 class BasicBFSFinder(IBFSFinder):
-    """Finds initial BFS assuming all constraints are of the form Ax = b, x >= 0."""
+    """Finds initial BFS assuming slack variables form the initial basis."""
     def find_initial_bfs(self, standard_form: LPProblem) -> BFSolution:
         m = len(standard_form.constraints)
         n = standard_form.variables_count
 
-        basis_indices = list(range(n, n + m))
+        basis_indices = list(range(n - m, n))
         basic_values = [c.free_val for c in standard_form.constraints]
 
-        full_solution = [0.0] * (n + m)
+        full_solution = [0.0] * n
         for i, bi in enumerate(basis_indices):
             full_solution[bi] = basic_values[i]
 
